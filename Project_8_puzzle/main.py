d14e1bda8beb36ee8ba40f3d597d6933442e0cb6
Tanund Sakpponsup <st123176@ait.asia>
2022-11-30 01:09:05 +0700

    move results to output folder

--- Project_8_puzzle/main.py
+++ Project_8_puzzle/main.py
@@ -12,24 +12,24 @@ def print_state(final_state:State, file_path):
     
     with open(file_path, 'w') as f:
         for i in range(len(list_state)):
-            print('----------------------------')
+            # print('----------------------------')
             f.write('----------------------------\n')
             if i == 0:
-                print('Step: ' + str(i) + ' --> Initial board')
+                # print('Step: ' + str(i) + ' --> Initial board')
                 f.write('Step: ' + str(i) + ' --> Initial board\n')
             elif i == len(list_state) - 1:
-                print('Step: ' + str(i) + ' --> Goal board')
+                # print('Step: ' + str(i) + ' --> Goal board')
                 f.write('Step: ' + str(i) + ' --> Goal board\n')
             else:
-                print('Step: ' + str(i))
+                # print('Step: ' + str(i))
                 f.write('Step: ' + str(i) + '\n')
 
-            print('Action: ' + str(list_state[i].action))
+            # print('Action: ' + str(list_state[i].action))
             f.write('Action: ' + str(list_state[i].action) + '\n')
-            print('Level: ' + str(list_state[i].g_score))
+            # print('Level: ' + str(list_state[i].g_score))
             f.write('Level: ' + str(list_state[i].g_score) + '\n')
             for row in list_state[i].board:
-                print(row)
+                # print(row)
                 f.write(str(row) + '\n')
 
         print('------Summary---------')
@@ -72,16 +72,16 @@ def isSolvable_new(init, goal) :
     return (inversion % 2 == 0)
 
 if __name__ == '__main__':
-    dir = os.path.dirname(__file__)
+    dir = os.path.dirname(__file__) + '\\output'
     goal_board = [
         [1,2,3],
-        [4,5,6],
-        [7,8,0]
+        [8,0,4],
+        [7,6,5]
         ]
     init_board = [
-        [1,2,3],
-        [0,4,6],
-        [7,5,8]
+        [2,8,3],
+        [1,6,4],
+        [7,0,5]
         ]
     if(isSolvable_new(init_board, goal_board)):
         print("Solvable")
@@ -90,18 +90,22 @@ if __name__ == '__main__':
         exit()
     init_state = State(goal_board, init_board, 0)
     
+    print('Running A_star')
     (final_state, count, running_time, max_level, remaining_state) = A_star(init_state)
     if final_state:
         print_state(final_state, dir + '\\A_star.txt')
 
+    print('Running Dijkstra')
     (final_state, count, running_time, max_level, remaining_state) = Dijkstra(init_state)
     if final_state:
         print_state(final_state, dir + '\\Dijkstra.txt')
-
+    
+    print('Running BFS')
     (final_state, count, running_time, max_level, remaining_state) = BFS(init_state)
     if final_state:
         print_state(final_state, dir + '\\BFS.txt')
 
+    print('Running DFS')
     (final_state, count, running_time, max_level, remaining_state) = DFS(init_state)
     if final_state:
         print_state(final_state, dir + '\\DFS.txt')
